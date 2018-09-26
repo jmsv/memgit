8b27b0646d64e2529e453e7513f712642013499f
James Vickery <dev@jamesvickery.net>
2018-09-26 13:48:33 +0100

    isomorphic-git and memfs code

---
+++ app.js
@@ -0,0 +1,31 @@
+var fsReal = require('fs')
+
+var fs = require('memfs').fs
+var vol = require('memfs').vol
+
+var git = require('isomorphic-git')
+
+git.plugins.set('fs', fs)
+
+async function main () {
+  await git.init({ dir: '/' })
+
+  fs.writeFileSync('/avocado.txt', 'avocado content')
+
+  await git.add({dir: '/', filepath: '/avocado.txt'})
+
+  await git.commit({
+    dir: '/',
+    author: {
+      name: 'James Vickery',
+      email: 'dev@jamesvickery.net'
+    },
+    message: 'added avocado'
+  })
+
+  const repoJson = vol.toJSON()
+
+  fsReal.writeFileSync('repo.json', JSON.stringify(repoJson, null, 2))
+}
+
+main()
